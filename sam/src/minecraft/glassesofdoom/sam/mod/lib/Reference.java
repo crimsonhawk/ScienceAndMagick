4884b14b217876a141a84fbe9cce077019eae8de
glassesofdoom <Glassesofdoom@gmail.com>
2013-02-06 22:55:32 -0500

    version change

--- sam/src/minecraft/glassesofdoom/sam/mod/lib/Reference.java
+++ sam/src/minecraft/glassesofdoom/sam/mod/lib/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	
 	public final static String MOD_ID = "S&M";
 	public final static String MOD_NAME = "Science & Magick";
-	public final static String Version = "0.1.0 Extream Alfa";
+	public final static String Version = "0.0.1 Extream Pre-Alfa";
 	public final static String ClientProxy = "glassesofdoom.sam.mod.core.proxys.ClientProxy";
 	public final static String CommonProxy = "glassesofdoom.sam.mod.core.proxys.CommonProxy";
 	
